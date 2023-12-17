9e8a26ea71641abcfd15a700d92688be0ceb15a3
Jahongir1202 <otadjonovjahongir8@gmail.com>
2023-12-17 23:50:13 +0500

    fix: requirements

--- config/settings.py
+++ config/settings.py
@@ -20,18 +20,16 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@$atf71%chf#^cs@e(p*ir8)%6$_uy9mj-c3cgrpysw*opz8*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG",default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = ["https://demo-app-36a3e430b76e.herokuapp.com",'127.0.0.1']
-
+ALLOWED_HOSTS = ['demo-app-36a3e430b76e.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -45,17 +43,12 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap4",
     "accounts",
-    # 'django.contrib.auth',
     "pages",
     "modeltranslation",
-    # "whitenoise.runserver_nostatic",
-
-
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhitenoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -84,7 +77,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -121,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -132,7 +123,6 @@ TIME_ZONE = "Asia/Tashkent"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -164,7 +154,6 @@ LANGUAGES = [
     # Boshqa til variantlari
 ]
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'uz'
-
 
 # LOCALE_PATHS = [
 #     os.path.join(BASE_DIR, 'locale'),  # locale papkasi yaratiladi
